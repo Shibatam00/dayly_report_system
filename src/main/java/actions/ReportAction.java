cd5a22202f3068c45a9958d179300cf7f6b70e99
Machiko.Shibata <tsya1235@gmail.com>
2022-10-30 11:57:10 +0900

    日報showアクションの作成

--- src/main/java/actions/ReportAction.java
+++ src/main/java/actions/ReportAction.java
@@ -6,13 +6,15 @@ import java.util.List;
 
 import javax.servlet.ServletException;
 
+import actions.views.EmployeeView;
 import actions.views.ReportView;
 import constants.AttributeConst;
 import constants.ForwardConst;
 import constants.JpaConst;
+import constants.MessageConst;
 import services.ReportService;
 
-public class ReportAction extends ActionBase{
+public class ReportAction extends ActionBase {
 
     private ReportService service;
 
@@ -24,7 +26,7 @@ public class ReportAction extends ActionBase{
         service.close();
     }
 
-    public void index() throws ServletException, IOException{
+    public void index() throws ServletException, IOException {
 
         int page = getPage();
         List<ReportView> reports = service.getAllPerPage(page);
@@ -37,14 +39,15 @@ public class ReportAction extends ActionBase{
         putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE);
 
         String flush = getSessionScope(AttributeConst.FLUSH);
-        if(flush != null) {
+        if (flush != null) {
             putRequestScope(AttributeConst.FLUSH, flush);
             removeSessionScope(AttributeConst.FLUSH);
         }
         forward(ForwardConst.FW_REP_INDEX);
     }
+
     //新規登録画面を表示
-    public void entryNew() throws ServletException, IOException{
+    public void entryNew() throws ServletException, IOException {
 
         putRequestScope(AttributeConst.TOKEN, getTokenId());
 
@@ -54,6 +57,54 @@ public class ReportAction extends ActionBase{
         putRequestScope(AttributeConst.REPORT, rv);
 
         forward(ForwardConst.FW_REP_NEW);
+    }
+
+    public void create() throws ServletException, IOException {
+
+        if (checkToken()) {
+
+            LocalDate day = null;
+            if (getRequestParam(AttributeConst.REP_DATE) == null
+                    || getRequestParam(AttributeConst.REP_DATE).equals("")) {
+                day = LocalDate.now();
+            } else {
+                day = LocalDate.parse(getRequestParam(AttributeConst.REP_DATE));
+            }
+            EmployeeView ev = (EmployeeView) getSessionScope(AttributeConst.LOGIN_EMP);
+
+            ReportView rv = new ReportView(
+                    null,
+                    ev,
+                    day,
+                    getRequestParam(AttributeConst.REP_TITLE),
+                    getRequestParam(AttributeConst.REP_CONTENT),
+                    null,
+                    null);
+            List<String> errors = service.create(rv);
+            if (errors.size() > 0) {
+                putRequestScope(AttributeConst.TOKEN, getTokenId());
+                putRequestScope(AttributeConst.REPORT, rv);
+                putRequestScope(AttributeConst.ERR, errors);
+
+                forward(ForwardConst.FW_REP_NEW);
+            } else {
+                putSessionScope(AttributeConst.FLUSH, MessageConst.I_REGISTERED.getMessage());
+                redirect(ForwardConst.ACT_REP, ForwardConst.CMD_INDEX);
+            }
+        }
+    }
+
+    public void show() throws ServletException, IOException {
+
+        ReportView rv = service.findOne(toNumber(getRequestParam(AttributeConst.REP_ID)));
+
+        if (rv == null) {
+            forward(ForwardConst.FW_ERR_UNKNOWN);
+        } else {
+            putRequestScope(AttributeConst.REPORT, rv);
+
+            forward(ForwardConst.FW_REP_SHOW);
+        }
     }
 
 }
